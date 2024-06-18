860b42aa2e4ebf14efda41fecda2d4dee0572cf6
matheuspdf <matheuslopes.pdf@gmail.com>
2024-06-18 20:22:22 -0300

    Iniciando o projeto de FastAPI

---
+++ tests/test_app.py
@@ -0,0 +1,12 @@
+from http import HTTPStatus
+
+from fastapi.testclient import TestClient
+
+from fast_api.app import app
+
+
+def test_read_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)  # Arrange - Organização do teste
+    response = client.get('/')  # Act - Ação do teste
+    assert response.status_code == HTTPStatus.OK  # assert
+    assert response.json() == {'message': 'Olá, Mund'}
