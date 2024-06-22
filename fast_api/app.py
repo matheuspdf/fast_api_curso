49993c878b96d877a6f6df24f86299fdd54efaf3
matheuspdf <matheuslopes.pdf@gmail.com>
2024-06-22 17:00:09 -0300

    Exercício aula 02

--- fast_api/app.py
+++ fast_api/app.py
@@ -1,6 +1,7 @@
 from http import HTTPStatus
 
 from fastapi import FastAPI
+from starlette.responses import HTMLResponse
 
 from fast_api.schemas import Message
 
@@ -10,3 +11,18 @@ app = FastAPI()
 @app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá, Mundo'}
+
+
+@app.get('/html/', status_code=HTTPStatus.OK, response_class=HTMLResponse)
+def read_deve_retornar_ok_e_ola_mundo_em_html():
+    conteudo_html = """
+    <html>
+      <head>
+        <title>Olá Mundo!</title>
+      </head>
+      <body>
+        <h1>Olá Mundo!</h1>
+      </body>
+    </html>
+    """
+    return conteudo_html
--- tests/test_app.py
+++ tests/test_app.py
@@ -10,3 +10,20 @@ def test_read_root_deve_retornar_ok_e_ola_mundo():
     response = client.get('/')  # Act - Ação do teste
     assert response.status_code == HTTPStatus.OK  # assert
     assert response.json() == {'message': 'Olá, Mundo'}
+
+
+def test_read_deve_retornar_ok_e_ola_mundo_em_html():
+    client = TestClient(app)
+    response = client.get('/html/')
+    assert response.status_code == HTTPStatus.OK
+    html_esperado = """
+    <html>
+      <head>
+        <title>Olá Mundo!</title>
+      </head>
+      <body>
+        <h1>Olá Mundo!</h1>
+      </body>
+    </html>
+    """
+    assert response.text == html_esperado
