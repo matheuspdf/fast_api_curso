2c6518e3fd564c88d3fd3b33125f362adcdac1fd
matheuspdf <matheuslopes.pdf@gmail.com>
2024-06-27 21:30:12 -0300

    Implementando rotas CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,19 +1,7 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_api.app import app
-
-
-def test_read_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)  # Arrange - Organização do teste
-    response = client.get('/')  # Act - Ação do teste
-    assert response.status_code == HTTPStatus.OK  # assert
-    assert response.json() == {'message': 'Olá, Mundo'}
-
-
-def test_read_deve_retornar_ok_e_ola_mundo_em_html():
-    client = TestClient(app)
+def test_read_deve_retornar_ok_e_ola_mundo_em_html(client):
     response = client.get('/html/')
     assert response.status_code == HTTPStatus.OK
     html_esperado = """
@@ -27,3 +15,65 @@ def test_read_deve_retornar_ok_e_ola_mundo_em_html():
     </html>
     """
     assert response.text == html_esperado
+
+
+def test_read_root_deve_retornar_ok_e_ola_mundo(client):
+    response = client.get('/')  # Act - Ação do teste
+    assert response.status_code == HTTPStatus.OK  # assert
+    assert response.json() == {'message': 'Olá, Mundo'}
+
+
+def test_create_user(client):
+    response = client.post(
+        '/users/',  # UserSchema
+        json={
+            'username': 'testusername',
+            'email': 'test@test.com',
+            'password': 'password',
+        },
+    )
+    assert response.status_code == HTTPStatus.CREATED
+
+    assert response.json() == {
+        'username': 'testusername',
+        'email': 'test@test.com',
+        'id': 1,
+    }
+
+
+def test_read_users(client):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'testusername',
+                'email': 'test@test.com',
+                'id': 1,
+            }
+        ]
+    }
+
+
+def test_update_user(client):
+    response = client.put(
+        '/users/1',
+        json={
+            'password': '123',
+            'username': 'testusername2',
+            'email': 'test@test.com',
+            'id': 1,
+        },
+    )
+
+    assert response.json() == {
+        'username': 'testusername2',
+        'email': 'test@test.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.json() == {'message': 'User deleted!'}
