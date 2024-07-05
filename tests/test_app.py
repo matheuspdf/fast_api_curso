54de55c19536975d4a3975b1a97deb9751aedbc2
matheuspdf <matheuslopes.pdf@gmail.com>
2024-07-05 10:37:48 -0300

    Exercícios - aula 03 close #1

--- tests/test_app.py
+++ tests/test_app.py
@@ -41,6 +41,18 @@ def test_create_user(client):
     }
 
 
+# Exercício 3 da aula 3
+def test_get_user_by_id(client):
+    response = client.get('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'id': 1,
+        'username': 'testusername',
+        'email': 'test@test.com',
+    }
+
+
 def test_read_users(client):
     response = client.get('/users/')
     assert response.status_code == HTTPStatus.OK
@@ -73,7 +85,35 @@ def test_update_user(client):
     }
 
 
+def test_update_user_not_found(client):
+    response = client.put(
+        '/users/2',
+        json={
+            'username': 'matheus',
+            'email': 'test@test.com',
+            'password': '12345',
+        },
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
+
+
 def test_delete_user(client):
     response = client.delete('/users/1')
 
     assert response.json() == {'message': 'User deleted!'}
+
+
+# Exercício 3 - aula 3
+def test_get_user_by_id_not_found(client):
+    response = client.get('/users/1')
+
+    assert response.json() == {'detail': 'User not found'}
+
+
+def test_delete_user_not_found(client):
+    response = client.delete(
+        '/users/2',
+    )
+    assert response.status_code == HTTPStatus.NOT_FOUND
+    assert response.json() == {'detail': 'User not found'}
